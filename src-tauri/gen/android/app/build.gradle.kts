a95f539dc6f8ab9a6f55cb6bb0dce0bdcbc07b93
TomsProject <44722363+TomsProject@users.noreply.github.com>
2026-05-11 20:14:54 +0800

    update min android sdk version

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
     defaultConfig {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
         applicationId = "com.projectoms.tplayer"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
